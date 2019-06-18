33657fd3c59f341131fb209138631d921b00063a
Nikola Georgiev <nikola@kvaba.com>
2019-06-18 17:37:28 +0300

    basic bodyparts detection implemented

--- src/js/SceneGameplay.js
+++ src/js/SceneGameplay.js
@@ -4,8 +4,7 @@
 2. declare bodyparts array: bodyparts[left_hand] = ahriman
 3. load spritesheets: left_hand[0] = ahriman anim; left_hand[1] = 
 4. write input controller
-5. on bodypart change, check if 
-
+5. on bodypart change, check if the same, if different - change, if different - check if all parts are with the same race and play video
 */
 
 var SceneGameplay = new Phaser.Scene('SceneGameplay');
@@ -15,7 +14,14 @@ SceneGameplay.init = function() {
     this.videos = ['AHRIMAN', 'ANIMA', 'ENTROPIA', 'GENEZA', 'MATERIA', 'SAPIENS', 'SYSTEMA'];
     this.bodyparts = ['HEAD', 'ARM_LEFT', 'ARM_RIGHT', 'LEG_LEFT', 'LEG_RIGHT', 'BODY', 'CROTCH'];
     this.myStatics = [];
-    // this.spritesheets = [][];
+    // this.spritesheets = ['HEAD']['AHRIMAN'];
+    
+    // randomize starting body parts
+    for (i = 0; i < this.bodyparts.length; i++) {
+        this.bodyparts[i] = getRandomElement(this.races);
+        // this.bodyparts[i] = "AHRIMAN";
+    }
+    console.log(this.bodyparts)
 }
 
 SceneGameplay.preload = function() {
@@ -29,10 +35,11 @@ SceneGameplay.create = function() {
     this.scene.bringToTop('SceneGameplay'); // BRING GAMEPLAY SCENE TO TOP AFTER THE ASSETS HAVE LOADED
     this.setupAnims();
     this.setupLevel();
+    this.setupKeyboardControlls();
 }
 
 SceneGameplay.update = function() {
-
+    
 }
 
 SceneGameplay.setupAnims = function() {
@@ -48,9 +55,53 @@ SceneGameplay.setupAnims = function() {
 }
 
 SceneGameplay.setupLevel = function() {
-    this.myStatics.asd = this.add.sprite(0, 0, 'frame', 'FRAME_01.png')
+    this.sceneFrame = this.add.sprite(0, 0, 'frame', 'FRAME_01.png')
     .setOrigin(.5,.5)
     .setPosition(game.config.width*.5)
     .setScale(1.08,1.08);
-    this.myStatics.asd.play('frameAnim');
+    this.sceneFrame.play('frameAnim');
+    
+    
+}
+
+SceneGameplay.setupKeyboardControlls = function () {
+    this.input.keyboard.on('keydown-' + 'A', function (event) { 
+        this.buttonPressed(0);
+    }.bind(this));
+    this.input.keyboard.on('keydown-' + 'S', function (event) { 
+        this.buttonPressed(1);
+    }.bind(this));
+    this.input.keyboard.on('keydown-' + 'D', function (event) { 
+        this.buttonPressed(2);
+    }.bind(this));
+    this.input.keyboard.on('keydown-' + 'F', function (event) { 
+        this.buttonPressed(3);
+    }.bind(this));
+    this.input.keyboard.on('keydown-' + 'G', function (event) { 
+        this.buttonPressed(4);
+    }.bind(this));
+    this.input.keyboard.on('keydown-' + 'H', function (event) { 
+        this.buttonPressed(5);
+    }.bind(this));
+    this.input.keyboard.on('keydown-' + 'J', function (event) { 
+        this.buttonPressed(6);
+    }.bind(this));
+}
+
+SceneGameplay.buttonPressed = function (i) {
+    this.bodyparts[i] = this.races[(this.races.indexOf(this.bodyparts[i]) + 1) % this.races.length]
+    console.log(this.bodyparts)
+    console.log(checkIfEqual(this.bodyparts));
+}
+
+function checkIfEqual (bodyparts) {
+    var result = true;
+    for (i = 1; i < bodyparts.length; i++) {
+        if (bodyparts[i] != bodyparts[0]) result = false;
+    }
+    return result;
+}
+
+function getRandomElement(arr) {
+    return arr[Math.floor(Math.random()*arr.length)];
 }
